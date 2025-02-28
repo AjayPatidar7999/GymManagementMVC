e6c9a266b52092518a3aff36be17ee99c9e0b7cf
AjayPatidar7999 <akpatidar4@gmail.com>
2025-03-01 00:18:21 +0530

    gymmvcRBAC

---
+++ src/main/java/com/aithinkers/gymmvc/Security/SecurityConfig.java
@@ -0,0 +1,51 @@
+
+package com.aithinkers.gymmvc.Security;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/").hasAnyRole("MEMBER","TRAINER","ADMIN")
+                                .requestMatchers("/trainers/**").hasAnyRole("TRAINER","ADMIN")
+                                .requestMatchers("/workoutplans/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(logout -> logout.permitAll()
+                )
+                .exceptionHandling(configurer ->
+                configurer.accessDeniedPage("/access-denied")
+        );
+
+        return http.build();
+    }
+ 
+
+}
